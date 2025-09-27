f5fbf3e51e96d0a27e79a814a3feeb20c905c17a
AK-Git508 <aariz.mkgb@gmail.com>
2025-09-27 12:36:56 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,42 @@
+console.log("Hello World");
+
+const btn = document.querySelector(".btn-open");
+const form = document.querySelector(".fact-form");
+
+btn.addEventListener("click", function () {
+  if (form.classList.contains("hidden")) {
+    form.classList.remove("hidden");
+    btn.textContent = "Close";
+  } else {
+    form.classList.add("hidden");
+    btn.textContent = "Share a fact";
+  }
+});
+
+/*
+let votesInteresting = 23;
+let votesMindblowing = 5;
+const test = "Libson is the capital of Portugal";
+*/
+
+function calcFactAge(year) {
+  const currentYear = new Date().getFullYear();
+  const age = currentYear - year;
+
+  if (age >= 0) return age;
+  else return "This year is not possible";
+}
+
+const age1 = calcFactAge(2015);
+console.log(age1);
+
+let votesInteresting = 20;
+let votesMindblowing = 23;
+
+if (votesInteresting === votesMindblowing) {
+  alert("This fact is equally interesting and mindblowing");
+} else if (votesInteresting > votesMindblowing) {
+  console.log("Interesting Fact!");
+} else if (votesInteresting < votesMindblowing) {
+  console.log("Mindblowing Fact!!");
+}
